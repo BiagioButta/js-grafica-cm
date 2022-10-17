7fcc2ab9705fa9b69ec655410b51e0608d276b35
BiagioButta <biagiob940@gmail.com>
2022-10-17 16:39:38 +0200

    bonus completo senza bombe

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
 "use strict";
 
-let rowhtml = document.querySelector('.row');
+let grigliahtml = document.querySelector('.griglia');
 let playhtml = document.getElementById('play');
+let difficoltahtml = document.getElementById('difficolta');
+let personalizzahtml = document.getElementById('personalizza');
+let ascissehtml = document.getElementById('ascisse');
+let ordinatehtml = document.getElementById('ordinate');
 
+console.log(typeof 'ascissehtml')
+
+// applico la funzione creaCelle al click del tasto gioca
 playhtml.addEventListener('click',function(){
-    rowhtml.innerHTML= '';
-    creaCelle(10,10)
+
+    grigliahtml.innerHTML= '';
+    let valoreDiff = parseInt(difficoltahtml.value);
+
+    if(valoreDiff == 0){
+        grigliahtml.innerHTML='';
+    }else if(valoreDiff == 1){
+        creaCelle(9,9)
+    }else if(valoreDiff == 2){
+        creaCelle(16,16)
+    }else if(valoreDiff == 3){
+        creaCelle(30,16)
+    }
 })
 
+// applico la funzione creaCelle al tasto personalizza
+
+personalizzahtml.addEventListener('click', function(){
+    grigliahtml.innerHTML = '';
+    ascissehtml.innerHTML= '',
+    ordinatehtml.innerHTML = '';
+
+    let colonne = parseInt(ascissehtml.value);
+    let righe = parseInt(ordinate.value);
+
+    creaCelle(colonne, righe);
+})
 
 // funzione che crea celle x * y
 function creaCelle ( x, y ){
@@ -15,13 +45,17 @@ function creaCelle ( x, y ){
     const celleTot = x*y;
     
     for(let i = 1; i <= celleTot; i++){
+
         const cella = document.createElement('div');
         cella.className = 'coordinata';
         cella.classList.add('debug')
         cella.innerHTML = `${i}`;
-        rowhtml.append(cella);
-        cella.style.width = `calc(100% / ${x})`;
+        grigliahtml.append(cella);
+        // pongo x uguale all asse delle ascisse
+        // y sarà l'ordinata
+        cella.style.width = `calc( 100%  / ${x}) `;
         
+        // do il colore verde al click
         cella.addEventListener('click', function(){
             this.classList.add('bg-green')
         })
