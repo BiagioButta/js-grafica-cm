f22f22bce45ef107a907bf3728c28dcca832ad38
BiagioButta <biagiob940@gmail.com>
2022-10-17 14:12:46 +0200

    completo senza bonus

--- js/script.js
+++ js/script.js
@@ -3,20 +3,25 @@
 let rowhtml = document.querySelector('.row');
 let playhtml = document.getElementById('play');
 
+playhtml.addEventListener('click',function(){
+    rowhtml.innerHTML= '';
+    creaCelle(10,10)
+})
 
-
-function creaCelle ( x, y){
-
+function creaCelle ( x, y ){
+    
     const celleTot = x*y;
-
+    
     for(let i = 1; i <= celleTot; i++){
         const cella = document.createElement('div');
         cella.className = 'coordinata';
         cella.classList.add('debug')
         cella.innerHTML = `${i}`;
         rowhtml.append(cella);
-
+        
+        cella.addEventListener('click', function(){
+            this.classList.add('bg-green')
+        })
+        
     }
 }
-creaCelle(10,10)
-console.log(creaCelle)
